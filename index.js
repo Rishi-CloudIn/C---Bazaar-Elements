93b9837ddfa7200c7bf9c6bd8bb253f73bc1cb31
manoj-front-end-cloudin <110726018+manoj-front-end-cloudin@users.noreply.github.com>
2022-09-13 18:59:30 +0530

    add navbar and approved section

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".nav-ul");
+
+hamburger_menu.addEventListener( "click", () =>
+{
+  hamburger_menu.classList.toggle( "change" );
+  navbar.classList.toggle( "new-ul" );
+} );
+
+document.querySelectorAll( ".nav-li a" ).forEach( a => a.addEventListener( "click", () =>
+{
+  hamburger_menu.classList.remove( "change" );
+} ) );
+
